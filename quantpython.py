7d2fa26f85346fcb4b0a64467075249841fc2190
Marcin Brdys AI.data <106516588+bdysm@users.noreply.github.com>
2022-08-24 19:59:38 +0200

    Delete quantpython.py

--- quantpython.py
+++
@@ -1,9 +0,0 @@
-import QuantLib as ql
-
-
-unifMt = ql.MersenneTwisterUniformRng()
-bmGauss = ql.BoxMullerMersenneTwisterGaussianRng(unifMt)
-
-for i in range(5):
-    print(bmGauss.next().value())
-
